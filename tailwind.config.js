962a3f878f9e0f93873fc92bf431f1e62b53e207
Domains18 <gibsonsgibson88@gmail.com>
2024-04-01 22:52:19 +0300

    added reacr

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-    "./public/**/*.{html, js}", 
-    "./public/**/*"
-],
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      fontFamily: {
-        "headings": "'Poppins', sans-serif",
-        "body": "'Open Sans', sans-serif",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
-
