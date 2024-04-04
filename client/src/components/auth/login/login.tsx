488559549676f5660f18f09266550498a3d8ff98
Domains18 <gibsonsgibson88@gmail.com>
2024-04-04 21:23:29 +0300

    responsive design for auth

--- client/src/components/auth/login/login.tsx
+++ client/src/components/auth/login/login.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import './login.scss'
+
 
 const Login = () => {
-  return (
-    <div>
-        <h1>Login</h1>
-    </div>
-  )
+    return (
+        <div className="login bg-white">
+            <h1>Welcome Back</h1>
+            <form className="form">
+                <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" name="email"/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" name="password"/>
+                </div>
+                <button type="submit">Login</button>
+            </form>
+            <p>Don't have an account? <a href="signup">Signup</a></p>
+        </div>
+    )
 }
 
 export default Login
