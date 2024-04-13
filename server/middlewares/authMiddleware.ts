9677954a0a8cb681551781369ccf4087e24df639
Domains18 <gibsonsgibson88@gmail.com>
2024-04-14 00:50:40 +0300

    Refactor authMiddleware and router.ts files, and update dependencies

---
+++ server/middlewares/authMiddleware.ts
@@ -0,0 +1,42 @@
+// const jwt = require('jsonwebtoken');
+// const { PrismaClient } = require('@prisma/client');
+import jwt from 'jsonwebtoken';
+
+
+const Prisma = new PrismaClient();
+
+const protectRoutes = async (req, res, next) => {
+  try {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+    if (!token) {
+      return res.status(401).json({ error: 'Not authorized, no token found' });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET || 'secret');
+    let user;
+    switch (decoded.role) {
+      case 'ADMIN':
+        user = await Prisma.admin.findUnique({ where: { id: decoded.id } });
+        break;
+      case 'CUSTOMER':
+        user = await Prisma.customer.findUnique({ where: { id: decoded.id } });
+        break;
+      // case 'VENDOR':
+      //   user = await Prisma.vendor.findUnique({ where: { id: decoded.id } });
+      //   break;
+      default:
+        return res.status(404).json({ error: 'User not found' });
+    }
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(401).json({ error: 'Not authorized, token failed' });
+  }
+};
+
+module.exports = protectRoutes;
---
+++ server/routes/router.ts
@@ -0,0 +1,29 @@
+//router
+
+
+// const { Router } = require('express');
+// const { createCustomer, customerLogin, getACustomer, getACustomerByToken } = require('../controllers/customers.api.js');
+// const { createBusiness, getBusinessWithCustomerDetails, registerStore   }=require('../controllers/business.api.js');
+// const protectRoutes= require('../middlewares/authMiddleware');
+
+
+import { Router } from 'express';
+import { createCustomer, customerLogin, getACustomer, getACustomerByToken } from '../controllers/customers.api';
+import { createBusiness, } from '../controllers/business.api';
+import protectRoutes from '../middlewares/authMiddleware';
+
+
+const router = Router();
+
+router.post('/create-customer', createCustomer);
+router.post('/customer-login', customerLogin);
+router.get('/get-customer/:id', protectRoutes, getACustomer);
+router.get('/getByToken',  getACustomerByToken);
+
+
+// router.post('/create-business', protectRoutes, createBusiness);
+// router.get('/get-business/:id', protectRoutes, getBusinessWithCustomerDetails);
+// router.post('/register-store/:businessId', protectRoutes, registerStore);
+
+
+module.exports = router;
