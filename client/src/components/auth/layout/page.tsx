a5e3d523cd9f8cf6c55dde5a82d30cf022914044
Domains18 <gibsonsgibson88@gmail.com>
2024-04-14 15:32:50 +0300

    Update authentication layout to use full width in page.tsx and remove TODO comment

--- client/src/components/auth/layout/page.tsx
+++ client/src/components/auth/layout/page.tsx
@@ -6,7 +6,7 @@ const AuthLayout = () => {
     return (
         <>
             <Navbar/>
-            <div className=" bg-slate-800 ">  {/* TODO:potential ui break */}
+            <div className=" bg-slate-800 w-full h-screen">  {/* TODO:potential ui break */}
                 <Outlet/>
             </div>
         </>
